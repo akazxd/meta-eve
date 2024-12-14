6b6d57eaeb7329cdf1e0c871389cdc46db44b3d7
A K Azad <azadcommits@gmail.com>
2024-12-14 06:00:28 +0100

    radeon icd loader

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ fi
 
 export WINEARCH=win64
 # export PATH="$HOME/EVE/wine-9.15f/bin:$PATH"
+export VK_DRIVER_FILES=/usr/share/vulkan/icd.d/radeon_icd.x86_64.json
 export RADV_PERFTEST='aco,cswave32,dccmsaa,gewave32,pswave32,rtwave64,sam' RADV_TEX_ANISO=4
 export LD_BIND_NOW=1 ENABLE_VKBASALT=1 DXVK_HUD="fps,compiler" DXVK_LOG_LEVEL=none VKD3D_DEBUG=none VKBASALT_LOG_LEVEL=error
 export FREETYPE_PROPERTIES="truetype:interpreter-version=35"
