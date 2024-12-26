262a4af14a9a3847d42b30d07ad1e9230b1df1fe
A K Azad <azadcommits@gmail.com>
2024-12-26 03:39:35 +0100

    cleanup

--- run.sh
+++ run.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
+which wine
+
 if [ -z "${WINEPREFIX}" ]; then
     export WINEPREFIX=$HOME/EVE/eve-online
 fi
 
 export WINEARCH=win64
-# export PATH="$HOME/EVE/wine-9.15f/bin:$PATH"
-export VK_DRIVER_FILES=/usr/share/vulkan/icd.d/radeon_icd.x86_64.json
-export RADV_PERFTEST='aco,cswave32,dccmsaa,gewave32,pswave32,rtwave64,sam' RADV_TEX_ANISO=4
-export LD_BIND_NOW=1 ENABLE_VKBASALT=1 DXVK_HUD="fps,compiler" DXVK_LOG_LEVEL=none VKD3D_DEBUG=none VKBASALT_LOG_LEVEL=error
+export STAGING_SHARED_MEMORY=1 WINEFSYNC_FUTEX2=1 WINEFSYNC=1 WINEDEBUG="-all"
 export FREETYPE_PROPERTIES="truetype:interpreter-version=35"
 
-# export DISPLAY=
-export STAGING_SHARED_MEMORY=1 WINEFSYNC_FUTEX2=1 WINEFSYNC=1 WINEDEBUG="-all"
-# $HOME/EVE/wine/bin/
+export VK_DRIVER_FILES=/usr/share/vulkan/icd.d/radeon_icd.x86_64.json
+export RADV_PERFTEST='aco,cswave32,dccmsaa,gewave32,pswave32,rtwave64,sam' RADV_TEX_ANISO=4
+
+export VKD3D_CONFIG=force_host_cached VKD3D_DEBUG=none
+export DXVK_HUD="fps,compiler" DXVK_LOG_LEVEL=none
+export LD_BIND_NOW=1 ENABLE_VKBASALT=1 VKBASALT_LOG_LEVEL=error MANGOHUD=1
 
 if [ "$#" -eq 0 ]; then
-    wine explorer "%USERPROFILE%\AppData\Local\eve-online"
-else
-    wine start "%USERPROFILE%\AppData\Local\eve-online\eve-online.exe" "$@"
+    wineserver -k 15; sleep 3;
 fi
+gamemoderun wine start "%USERPROFILE%\AppData\Local\eve-online\eve-online.exe" "$@"
 
 # ps aux | grep eve-online | awk '{print $2}' | xargs -I{} kill -9 {}
+# ps aux | grep "wine...?" | awk '{print $2}' | xargs -I{} kill -9 {}
