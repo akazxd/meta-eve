e5c7510a63cbd59c5cbc3117e6f10b681b69efbe
A K Azad <azadcommits@gmail.com>
2025-02-25 17:41:01 +0100

    update

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export PATH="/home/aazad/makepkg/meta-eve/wine-10/bin:$PATH"
+which wine
+
 export WINEARCH=win64
 if [ -z "${WINEPREFIX}" ]; then
     export WINEPREFIX=$HOME/EVE/eve-online
@@ -12,11 +15,14 @@ rm -rf $WINEPREFIX/drive_c/users/$USER/Temp $WINEPREFIX/drive_c/windows/temp
 ln -s /tmp/ $WINEPREFIX/drive_c/users/$USER/Temp
 ln -s /tmp/ $WINEPREFIX/drive_c/windows/temp
 
-echo 'Installing dependencies'
-winetricks -q vcrun2022 dxvk vkd3d
-
 echo 'Check Staging > Enable VAAPI as backend for DXVA2 GPU Decoding'
 winecfg
+
+echo 'Installing dependencies'
+winetricks --self-update
+winetricks -q vcrun2022 #dxvk vkd3d
+setup_dxvk install
+setup_vkd3d_proton install
 
 echo 'Installing EVE Online'
 wine 'eve-online-latest+Setup.exe'
--- run.sh
+++ run.sh
@@ -7,23 +7,35 @@ if [ -z "${WINEPREFIX}" ]; then
     export WINEPREFIX=$HOME/EVE/eve-online
 fi
 
+# wine vars
 export WINEARCH=win64
 export STAGING_SHARED_MEMORY=1 WINEFSYNC_FUTEX2=1 WINEFSYNC=1 WINEDEBUG="-all"
 export FREETYPE_PROPERTIES="truetype:interpreter-version=35"
 
-export VK_DRIVER_FILES=/usr/share/vulkan/icd.d/radeon_icd.x86_64.json
+# radv vars
+export VK_DRIVER_FILES='/usr/share/vulkan/icd.d/radeon_icd.x86_64.json'
 export RADV_PERFTEST='aco,cswave32,dccmsaa,gewave32,pswave32,rtwave64,sam' RADV_TEX_ANISO=4
 
-export VKD3D_CONFIG=force_host_cached VKD3D_DEBUG=none VKD3D_FRAME_RATE=60 VK_MESA_image_alignment_control=1 VKD3D_SWAPCHAIN_LATENCY_FRAMES=2 RADV_DEBUG=novrsflatshading VKD3D_CONFIG=no_upload_hvv VKD3D_CONFIG=no_staggered_submit
-export DXVK_HUD="fps,compiler" DXVK_LOG_LEVEL=none
-export LD_BIND_NOW=1 ENABLE_VKBASALT=1 VKBASALT_LOG_LEVEL=error MANGOHUD=1
+# nvidia vars
+# export VK_DRIVER_FILES='/usr/share/vulkan/icd.d/nvidia_icd.json'
+# export __GL_LOG_MAX_ANISO=4 __GL_SHOW_GRAPHICS_OSD=1 MESA_GLSL_CACHE_MAX_SIZE=512 __NV_PRIME_RENDER_OFFLOAD=1 __GL_SYNC_TO_VBLANK=0
 
-# sudo scx_lavd
+# vkd3d vars
+export VKD3D_CONFIG=force_host_cached VKD3D_DEBUG=none VKD3D_FRAME_RATE=60 VK_MESA_image_alignment_control=1
+export VKD3D_SWAPCHAIN_LATENCY_FRAMES=2 RADV_DEBUG=novrsflatshading VKD3D_CONFIG=no_upload_hvv VKD3D_CONFIG=no_staggered_submit
+
+# dxvk vars
+export DXVK_STATE_CACHE=1 DXVK_SHADER_OPTIMIZE=1 WINE_DISABLE_WRITE_WATCH=1 DXVK_FRAME_RATE=60 DXVK_HUD="compiler" DXVK_LOG_LEVEL=none
+export DXVK_CONFIG_FILE="$HOME/makepkg/meta-eve/dxvk.conf"
+
+# extra vars
+export LD_BIND_NOW=1 ENABLE_VKBASALT=1 VKBASALT_LOG_LEVEL=error MANGOHUD=1
 
 if [ "$#" -eq 0 ]; then
     wineserver -k 15; sleep 3;
 fi
-gamemoderun wine start "%USERPROFILE%\AppData\Local\eve-online\eve-online.exe" "$@"
+gamemoderun wine64 start "%USERPROFILE%\AppData\Local\eve-online\eve-online.exe" "$@"
 
+# sudo scx_lavd
 # ps aux | grep eve-online | awk '{print $2}' | xargs -I{} kill -9 {}
 # ps aux | grep "wine...?" | awk '{print $2}' | xargs -I{} kill -9 {}
